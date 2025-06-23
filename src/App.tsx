f5eadbeb8c96f6810b8f4671fc5a9fa4ff6336ad
HaoSugoi <truonghao2406@gmail.com>
2025-06-23 22:24:37 +0700

    Admin DashBoard

--- src/App.tsx
+++ src/App.tsx
@@ -36,19 +36,19 @@ function App() {
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-            <Route path="/admin/books" element={<ProtectedRoute><BooksManagement /></ProtectedRoute>} />
-            <Route path="/admin/authors" element={<ProtectedRoute><AuthorsManagement /></ProtectedRoute>} />
-            <Route path="/admin/customers" element={<ProtectedRoute><CustomersManagement /></ProtectedRoute>} />
+             <Route path="/admin/customers" element={<ProtectedRoute><CustomersManagement /></ProtectedRoute>} />
             <Route path="/admin/employees" element={<ProtectedRoute><EmployeesManagement /></ProtectedRoute>} />
-            <Route path="/admin/orders" element={<ProtectedRoute><OrdersManagement /></ProtectedRoute>} />
+            <Route path="/admin/reports" element={<ProtectedRoute><ReportsManagement /></ProtectedRoute>} />
+            <Route path="/admin/books" element={<ProtectedRoute><BooksManagement /></ProtectedRoute>} />
             <Route path="/admin/rentals" element={<ProtectedRoute><RentalManagement /></ProtectedRoute>} />
+            <Route path="/admin/authors" element={<ProtectedRoute><AuthorsManagement /></ProtectedRoute>} />
+            <Route path="/admin/orders" element={<ProtectedRoute><OrdersManagement /></ProtectedRoute>} />
             <Route path="/admin/promotions" element={<ProtectedRoute><PromotionsManagement /></ProtectedRoute>} />
             <Route path="/admin/discount-codes" element={<ProtectedRoute><DiscountCodesManagement /></ProtectedRoute>} />
-            <Route path="/admin/shipping" element={<ProtectedRoute><ShippingManagement /></ProtectedRoute>} />
-            <Route path="/admin/users" element={<ProtectedRoute><UsersManagement /></ProtectedRoute>} />
-            <Route path="/admin/roles" element={<ProtectedRoute><RolesManagement /></ProtectedRoute>} />
-            <Route path="/admin/reports" element={<ProtectedRoute><ReportsManagement /></ProtectedRoute>} />
-            <Route path="/admin/stats" element={<ProtectedRoute><StatsManagement /></ProtectedRoute>} />
+            {/* <Route path="/admin/shipping" element={<ProtectedRoute><ShippingManagement /></ProtectedRoute>} /> */}
+            {/* <Route path="/admin/users" element={<ProtectedRoute><UsersManagement /></ProtectedRoute>} /> */}
+            {/* <Route path="/admin/roles" element={<ProtectedRoute><RolesManagement /></ProtectedRoute>} /> */}
+            {/* <Route path="/admin/stats" element={<ProtectedRoute><StatsManagement /></ProtectedRoute>} /> */}
             {/* Route NotFound để cuối */}
             <Route path="*" element={<NotFound />} />
           </Routes>
