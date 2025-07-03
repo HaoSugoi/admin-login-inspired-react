f0f91d6f7c2490228f7a8cbab8404053322edd08
HaoSugoi <truonghao2406@gmail.com>
2025-07-03 18:54:00 +0700

    fix some thing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,7 @@
-
 import AdminLogin from '../components/AdminLogin.jsx';
-import SlideBanner from '../components/SlideBanner.jsx';
 
 const Index = () => {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      {/* Banner Section */}
-      <div className="container mx-auto px-4 py-8">
-        <SlideBanner />
-      </div>
-      
-      {/* Login Section */}
-      <AdminLogin />
-    </div>
-  );
+  return <AdminLogin />;
 };
 
 export default Index;
